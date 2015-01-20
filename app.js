fee18b59d5e6573bd743660e54c93e414de2a5be
Alex Vidal Rodriguez <goofyahead@gmail.com>
2015-01-20 17:20:44 +0100

    now is pub test

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-var colors = require('colors');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: "publisher"});
 
-console.log('hello'.green);
+// pubber.js
+var zmq = require('zmq')
+  , sock = zmq.socket('pub');
+
+sock.bindSync('tcp://127.0.0.1:3000');
+log.info('Publisher bound to port 3000');
+
+setInterval(function(){
+  log.info('sending a multipart message envelope');
+  sock.send(['kitty cats', 'meow!']);
+}, 500);
